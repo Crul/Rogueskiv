9f8b06c521b8885db2a33fbde9e44fe9c47054cc
Crul <rauljavier.vila@gmail.com>
2019-12-14 19:10:35 +0100

    walls generation added

--- Rogueskiv.Core/RogueskivGame.cs
+++ Rogueskiv.Core/RogueskivGame.cs
@@ -1,5 +1,4 @@
 ﻿using Rogueskiv.Core.Components;
-using Rogueskiv.Core.Components.Walls;
 using Rogueskiv.Core.Systems;
 using Rogueskiv.Engine;
 using System.Collections.Generic;
@@ -17,50 +16,7 @@ namespace Rogueskiv.Core
                         new PositionComp(){ X = 90, Y = 90 },
                         new MovementComp()
                     },
-
                     new List<IComponent> { new BoardComp(boardData) },
-
-                    new List<IComponent> { new RightWallComp(2, 2, 4)},
-
-                    new List<IComponent> { new DownWallComp(2, 2, 7)},
-                    new List<IComponent> { new LeftWallComp(9, 2, 1)},
-                    new List<IComponent> { new LeftWallComp(9, 4, 2)},
-                    new List<IComponent> { new UpWallComp(2, 6, 2)},
-                    new List<IComponent> { new UpWallComp(7, 6, 2)},
-
-                    new List<IComponent> { new RightWallComp(4, 6, 6)},
-                    new List<IComponent> { new LeftWallComp(7, 6, 6)},
-
-                    new List<IComponent> { new RightWallComp(2, 12, 4)},
-                    new List<IComponent> { new DownWallComp(2, 12, 2)},
-                    new List<IComponent> { new DownWallComp(7, 12, 2)},
-                    new List<IComponent> { new LeftWallComp(9, 12, 1)},
-                    new List<IComponent> { new LeftWallComp(9, 14, 2)},
-                    new List<IComponent> { new UpWallComp(2, 16, 7)},
-
-                    new List<IComponent> { new DownWallComp(9, 3, 10)},
-                    new List<IComponent> { new RightWallComp(18, 4, 1)},
-                    new List<IComponent> { new UpWallComp(9, 4, 9)},
-
-                    new List<IComponent> { new DownWallComp(10, 5, 8)},
-                    new List<IComponent> { new RightWallComp(10, 5, 6)},
-                    new List<IComponent> { new UpWallComp(10, 11, 1)},
-                    new List<IComponent> { new UpWallComp(12, 11, 7)},
-                    new List<IComponent> { new LeftWallComp(19, 3, 8)},
-
-                    new List<IComponent> { new RightWallComp(11, 11, 1)},
-                    new List<IComponent> { new LeftWallComp(12, 11, 1)},
-
-                    new List<IComponent> { new DownWallComp(9, 13, 1)},
-                    new List<IComponent> { new UpWallComp(9, 14, 1)},
-
-                    new List<IComponent> { new RightWallComp(10, 12, 1)},
-                    new List<IComponent> { new RightWallComp(10, 14, 1)},
-                    new List<IComponent> { new LeftWallComp(13, 12, 3)},
-
-                    new List<IComponent> { new DownWallComp(10, 12, 1)},
-                    new List<IComponent> { new DownWallComp(12, 12, 1)},
-                    new List<IComponent> { new UpWallComp(10, 15, 3)},
                 },
                 systems: new List<ISystem> {
                     new MovementSys(),
--- Rogueskiv.Core/Systems/BoardSys.cs
+++ Rogueskiv.Core/Systems/BoardSys.cs
@@ -1,5 +1,7 @@
 ﻿using Rogueskiv.Core.Components;
+using Rogueskiv.Core.Components.Walls;
 using Rogueskiv.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,26 +17,113 @@ namespace Rogueskiv.Core.Systems
                 .Single();
 
             var board = boardEntity.GetComponent<BoardComp>().Board;
+            var width = board[0].Length;
+            var height = board.Count;
 
-            AddTiles(game, board);
+            AddTiles(game, board, width, height);
+            // TODO fix empty cell needed around the board to make all walls
+            AddUpWalls(game, board, width, height);
+            AddDownWalls(game, board, width, height);
+            AddLeftWalls(game, board, width, height);
+            AddRightWalls(game, board, width, height);
 
             game.Entities.Remove(boardEntity);
 
             return false;
         }
 
-        private void AddTiles(Game game, List<string> board)
+        private static void AddUpWalls(Game game, List<string> board, int width, int height) =>
+            AddWalls(
+                game, width, height,
+                isWall: (x, y) => IsTile(board, x, y - 1) && !IsTile(board, x, y),
+                createComponent: wall => new UpWallComp(wall.x, wall.y, wall.size),
+                initHeight: 1
+            );
+
+        private static void AddDownWalls(Game game, List<string> board, int width, int height) =>
+            AddWalls(
+                game, width, height,
+                isWall: (x, y) => !IsTile(board, x, y - 1) && IsTile(board, x, y),
+                createComponent: wall => new DownWallComp(wall.x, wall.y, wall.size),
+                initHeight: 1
+            );
+
+        private static void AddLeftWalls(Game game, List<string> board, int width, int height) =>
+            AddWalls(
+                game, width, height,
+                isWall: (x, y) => !IsTile(board, x, y) && IsTile(board, x - 1, y),
+                createComponent: wall => new LeftWallComp(wall.x, wall.y, wall.size),
+                initWidth: 1
+            );
+
+        private static void AddRightWalls(Game game, List<string> board, int width, int height) =>
+            AddWalls(
+                game, width, height,
+                isWall: (x, y) => !IsTile(board, x - 1, y) && IsTile(board, x, y),
+                createComponent: wall => new RightWallComp(wall.x, wall.y, wall.size),
+                initWidth: 1
+            );
+
+        private static void AddWalls(
+            Game game,
+            int width,
+            int height,
+            Func<int, int, bool> isWall,
+            Func<(int x, int y, int size), IComponent> createComponent,
+            int initWidth = 0,
+            int initHeight = 0
+        )
         {
-            var width = board[0].Length;
-            var height = board.Count;
-            for (var x = 0; x < width; x++)
-                for (var y = 0; y < height; y++)
-                    if (IsTile(board, x, y))
-                        game.AddEntity(new TileComp(x, y));
+            (int x, int y, int size)? tmpWall = null;
+
+            for (var y = initHeight; y < height; y++)
+            {
+                for (var x = initWidth; x < width; x++)
+                {
+                    if (isWall(x, y))
+                    {
+                        if (tmpWall.HasValue)
+                            tmpWall = ExtendWall(tmpWall.Value);
+
+                        else
+                            tmpWall = (x, y, 1);
+                    }
+                    else if (tmpWall.HasValue)
+                    {
+                        game.AddEntity(createComponent(tmpWall.Value));
+                        tmpWall = null;
+                    }
+                }
+
+                if (tmpWall.HasValue)
+                {
+                    game.AddEntity(createComponent(tmpWall.Value));
+                    tmpWall = null;
+                }
+            }
         }
 
-        private bool IsTile(List<string> board, int x, int y) =>
+        private static void AddTiles(Game game, List<string> board, int width, int height)
+        {
+            ForAllCells(width, height, (x, y) =>
+            {
+                if (IsTile(board, x, y))
+                    game.AddEntity(new TileComp(x, y));
+            });
+        }
+
+        private static bool IsTile(List<string> board, int x, int y) =>
             board[y].Substring(x, 1) == "T";
+
+        private static void ForAllCells(int width, int height, Action<int, int> action)
+        {
+            for (var y = 0; y < height; y++)
+                for (var x = 0; x < width; x++)
+                    action(x, y);
+        }
+
+        private static (int x, int y, int size)? ExtendWall((int x, int y, int size) wall) =>
+            (wall.x, wall.y, wall.size + 1);
 
         public override void Update(List<IEntity> entities) { }
     }
