45bcacc0e1dc4dcf7f9251b7a05354bb16129c3a
Crul <rauljavier.vila@gmail.com>
2019-12-31 17:23:01 +0100

    controls info added

--- Rogueskiv.Menus/InstructionsRenderer.cs
+++ Rogueskiv.Menus/InstructionsRenderer.cs
@@ -10,23 +10,22 @@ namespace Rogueskiv.Menus
 {
     class InstructionsRenderer : IRenderer
     {
-        private const int MIN_MARGIN_X = 20;
+        private const int MIN_MARGIN_X = 40;
         private const int MAX_MARGIN_DELTA_X = 140 + MIN_MARGIN_X;
         private const int MIN_Y = 320;
-        private const int MARGIN_Y = 60;
+        private const int MARGIN_Y = 24;
         private const int LINE_HEIGHT = 24;
-        private const float MIN_SCREEN_WIDTH_TO_ADJUST_MARGIN = 1000;
+        private const float MIN_SCREEN_WIDTH_TO_ADJUST_MARGIN = 720;
         private const float MAX_SCREEN_WIDTH_TO_ADJUST_MARGIN = 1200;
         private readonly List<string> InstructionsTextLines = new List<string>
         {
             "Only keyboard supported (no mouse)",
             "Use cursors and press ENTER to select an option.",
-            "While playing, use the cursors to move and ESC key to pause and Q to exit).",
             "WARNING! Work in progress: This is a proof of concept, not a fully developed game."
         };
 
         private readonly SDL_Color InstructionsColor =
-            new SDL_Color() { r = 0xDD, g = 0xDD, b = 0xDD, a = 0xFF };
+            new SDL_Color() { r = 0xCC, g = 0xCC, b = 0xCC, a = 0xFF };
 
         protected readonly UxContext UxContext;
         protected readonly TextRenderer TextRenderer;
